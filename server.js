58882de5af69d0c2ab94eb326a37a142b0932fce
Gabszy24 <corderogabrielle@gmail.com>
2023-06-12 00:07:15 +0800

    x

--- server.js
+++ server.js
@@ -11,30 +11,6 @@ const db = require("./models");
 // LOGGER
 const morgan = require("morgan");
 
-app.get("*", async (req, res) => {
-  let filename = req.path.slice(1);
-
-  try {
-    let s3File = await s3
-      .getObject({
-        Bucket: process.env.BUCKET,
-        Key: filename,
-      })
-      .promise();
-
-    res.set("Content-type", s3File.ContentType);
-    res.send(s3File.Body.toString()).end();
-  } catch (error) {
-    if (error.code === "NoSuchKey") {
-      console.log(`No such key ${filename}`);
-      res.sendStatus(404).end();
-    } else {
-      console.log(error);
-      res.sendStatus(500).end();
-    }
-  }
-});
-
 // Middlewares
 app.use(express.json({ extended: false })); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -66,6 +42,31 @@ app.use(
 );
 app.use("/api/admin/category", require("./routes/admin/category.route"));
 app.use("/api/admin/order", require("./routes/admin/order.route"));
+
+
+app.get("/s3/*", async (req, res) => {
+  let filename = req.path.slice(1);
+
+  try {
+    let s3File = await s3
+      .getObject({
+        Bucket: process.env.BUCKET,
+        Key: filename,
+      })
+      .promise();
+
+    res.set("Content-type", s3File.ContentType);
+    res.send(s3File.Body.toString()).end();
+  } catch (error) {
+    if (error.code === "NoSuchKey") {
+      console.log(`No such key ${filename}`);
+      res.sendStatus(404).end();
+    } else {
+      console.log(error);
+      res.sendStatus(500).end();
+    }
+  }
+});
 
 const PORT = process.env.PORT;
 
